2d894978fc56fb359bc72203e2e471f2300b5fad
Eteor <radektrzeciak12@gmail.com>
2021-07-10 16:51:33 +0200

    opisane przyciski

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,9 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 
 public class Controller {
 
@@ -17,6 +20,38 @@ public class Controller {
   System.out.println(samochod.currentSpeed);
   label_result.setText(Integer.toString(samochod.currentSpeed));
  }
+
+ @FXML
+ ImageView displaySign;
+ Image ograniczenie30 = new Image(getClass().getResourceAsStream("ograniczenie30.jpg"));
+ Image ograniczenie50 = new Image(getClass().getResourceAsStream("ograniczenie50.jpg"));
+ Image ograniczenie90 = new Image(getClass().getResourceAsStream("ograniczenie120.jpg"));
+ Image pierszenstwo = new Image(getClass().getResourceAsStream("pierszenstwo.jpg"));
+ Image stop = new Image(getClass().getResourceAsStream("stop.jpg"));
+
+
+ public void sign1act()
+ {
+  displaySign.setImage(ograniczenie30);
+ }
+ public void sign2act()
+ {
+  displaySign.setImage(ograniczenie50);
+ }
+ public void sign3act()
+ {
+  displaySign.setImage(ograniczenie90);
+ }
+ public void sign4act()
+ {
+  displaySign.setImage(pierszenstwo);
+ }
+ public void sign5act()
+ {
+  displaySign.setImage(stop);
+ }
+
+
 
  /*public void CheckClick() throws InterruptedException {
   samochod.looseSpeed();
--- src/sample/car.java
+++ src/sample/car.java
@@ -3,21 +3,26 @@ package sample;
 import java.util.concurrent.TimeUnit;
 
 public class car {
+    int limit = 120;
 
-    int currentSpeed=0;
+    int currentSpeed = 0;
 
 
-    public void accelerate()
-    {
-        currentSpeed+=3;
+    public void accelerate() {
+
+        if (currentSpeed <= limit - 3) {
+            currentSpeed += 3;
+        } else {
+            currentSpeed = limit;
+        }
     }
-    public void breaking()
-    {
-        if(currentSpeed>7)
-        currentSpeed-=7;
-        else if ((currentSpeed<7)&(currentSpeed>0))
-            currentSpeed-=1;
-        else;
+
+    public void breaking() {
+        if (currentSpeed > 7) {
+            currentSpeed -= 7;
+        } else {
+            currentSpeed = 0;
+        }
     }
 
    /* public void looseSpeed() throws InterruptedException {
